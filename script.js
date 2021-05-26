199e8723147f5af13d90be66927fda7eece707e9
ILKooper <81315071+ILKooper@users.noreply.github.com>
2021-05-26 17:09:37 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+var startTime = new Date().getTime();
+
+var makeShapeVisible = function(){
+  var shape = document.getElementById("shape");
+  var top = Math.random() * 200;
+  var left = Math.random() * 200;
+  var width = Math.random() * 100 + 50;
+  shape.style.top = top +"px";
+  shape.style.left = left +"px";
+  shape.style.width = width + "px";
+
+  if(Math.random() < 0.5) {
+    shape.style.borderRadius = "50%";
+    } else {
+    shape.style.borderRadius = "0";
+  }
+
+  shape.style.display = "block";
+  startTime = new Date().getTime();
+}
+
+setTimeout(makeShapeVisible, Math.random() * 1000);
+
+document.getElementById("shape").onclick = function(){
+var shape = this;
+  
+  shape.style.display = "none";
+
+  var finishTime = new Date().getTime();
+  var reactionTime = (finishTime - startTime) / 1000;
+  document.getElementById("reactionTime").innerHTML = reactionTime + "seconds";
+  setTimeout(makeShapeVisible, Math.random() * 1000);
+}
